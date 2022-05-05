7ed39192c4a788e541501ed03625078e0ca02968
DuongMinhLoc <duongminhloc201@gmail.com>
2022-05-05 20:50:32 +0700

    Loc | 😵 | Add comments for workflow of add/update/remove customer in CustomerController

--- backend/src/main/java/com/pdm/pdm/booking/Customer/CustomerController.java
+++ backend/src/main/java/com/pdm/pdm/booking/Customer/CustomerController.java
@@ -12,6 +12,28 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+    // Sign In
+    // 1. User go to homepage url"/"
+    // 2. User press sign in button -> button go to url"/signing"
+    // 3. "/signing" will show the CustomerForm
+    // 4. CustomerForm has action to url"/signing/save" -> This url will save customer info
+    // 5. Redirect to url"/booking" which will show booking options
+
+    // --------------------------- //
+
+    // Remove Customer
+    // 1. User want to delete this account -> Press button "Delete Account" in Booking page or any possible pages
+    // 2. The "Delete Account" button will link to url"customer/remove/{id}" ({id} get in Front-end)
+    // 3. If delete successfully -> go to homepage url"/" else redirect to booking page url"/booking"
+
+    // --------------------------- //
+
+    // Update Customer
+    // 1. 1. User want to update this account -> Press button "Edit Info" in Booking page or any possible pages
+    // 2. The "Edit Info" button will link to url"customer/update/{id}" ({id} get in Front-end)
+    // 3. If getting the customer successfully -> go to CustomerForm with foundById-customer
+    //                                            else go back to booking page url"/booking"
+
     @GetMapping("/signing")
     public String showForm(Model model) {
         model.addAttribute("customer", new Customer());
