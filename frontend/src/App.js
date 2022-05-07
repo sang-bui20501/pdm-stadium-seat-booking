0bbf2628ff933f169eac4aae163d0c8491b28e3c
luongphucdien <work.luongphucdien@gmail.com>
2022-05-07 12:12:44 +0700

    Dien | ➕ | Add NavLink and Routes in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div className='header'>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/signup'>Sign Up</NavLink>
+          <NavLink to='/signin'>Sign In</NavLink>
+          <NavLink to='/book'>Book your seats</NavLink>
+        </div>
+
+        <div className='content'>
+          <Routes>
+            <Route path='/' />
+            <Route path='/signup' />
+            <Route path='/signin' />
+            <Route path='/book' />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
