d113a70d6544c200addaa8df24e3b73cd589bb13
Sang bui <goldensangvn@gmail.com>
2022-05-08 18:44:35 +0700

    Sang | Sign in| Add authentication logic into PrivateRoute

--- frontend/src/routes/private-route.js
+++ frontend/src/routes/private-route.js
@@ -2,11 +2,20 @@ import React from 'react'
 import { Navigate, Route, useLocation } from 'react-router-dom'
 import useAuth from 'hooks/use-auth';
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const PrivateRoute = ({ Component }) => {
-    const auth = useAuth()
+    const isAuthenticated = useAuth()
     const { location } = useLocation() 
-    if(!auth) {
+    const { navigate } = useNavigate()
+
+    useEffect(() => {
+        if(isAuthenticated) return
+
+        navigate({ path: "/login"})
+    }, [navigate, location, isAuthenticated])
+
+    if(!isAuthenticated) {
         return <Navigate to = '/login' state={{ from: location }} />
     }   
     return <Component />;
