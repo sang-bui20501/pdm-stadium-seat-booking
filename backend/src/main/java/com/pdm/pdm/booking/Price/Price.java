4875f7061e95db2dbcc561839b1096caa2831df2
nhathuy19135 <74639606+nhathuy19135@users.noreply.github.com>
2022-05-08 15:26:29 +0700

    Huy | :new_moon_with_face: | Added Price

---
+++ backend/src/main/java/com/pdm/pdm/booking/Price/Price.java
@@ -0,0 +1,45 @@
+package com.pdm.pdm.booking.Price;
+
+import javax.persistence.*;
+
+@Entity
+public class Price {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int priceId;
+
+    private int unit;
+
+    private int rate;
+
+    public Price(int priceId, int unit, int rate){
+        this.priceId = priceId;
+        this.unit = unit;
+        this.rate = rate;
+    }
+
+    public int getPriceId() {
+        return priceId;
+    }
+
+    public void setPriceId(int price) {
+        this.priceId = price;
+    }
+
+    public int getUnit() {
+        return unit;
+    }
+
+    public void setUnit(int unit) {
+        this.unit = unit;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+}
