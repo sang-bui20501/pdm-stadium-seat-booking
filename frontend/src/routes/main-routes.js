fe1c6854b90281ec36480e59d4edcd6ede983b7d
Sang bui <goldensangvn@gmail.com>
2022-05-08 18:38:09 +0700

    Sang | Sign in | Add Login logics

--- frontend/src/routes/main-routes.js
+++ frontend/src/routes/main-routes.js
@@ -4,11 +4,12 @@ import SignIn from '../pages/sign-in/sign-in'
 import SignUp from '../pages/sign-up/sign-up'
 import Homepage from '../pages/homepage/homepage'
 import ShowBookings from '../pages/show-bookings/show-bookings'
-import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import { getToken } from "utils/common"
+import PrivateRoute from './private-route';
 
 function MainRoutes() {
-  
+
   function requireAuth(nextState, replace) {
     if (!getToken()) {
       replace("/sign-in");
@@ -18,11 +19,11 @@ function MainRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" exact element={<Homepage/>}/>
-          <Route path="/sign-in" exact element={<SignIn/>}/>
-          <Route path="/sign-up" exact element={<SignUp/>}/>
-          <Route path="/booking" exact element={<BookingPage/>} />
-          <Route path='/your-bookings' exact element={<ShowBookings/>} onEnter={requireAuth}/>
+        <Route path="/" exact element={<PrivateRoute Component={Homepage} />} />
+        <Route path="/sign-in" exact element={<SignIn />} />
+        <Route path="/sign-up" exact element={<SignUp />} />
+        <Route path="/booking" exact element={<PrivateRoute Component={BookingPage} />} />
+        <Route path='/your-bookings' exact element={<PrivateRoute Component={ShowBookings} />} onEnter={requireAuth} />
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/routes/private-route.js
+++ frontend/src/routes/private-route.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { Navigate, Route, useLocation } from 'react-router-dom'
+import useAuth from 'hooks/use-auth';
+import { useNavigate } from 'react-router-dom';
 
-const PrivateRoute = ({ children, ...rest }) => {
-    return (
-        <Route
-            {...rest}
-            render={() => auth
-                ? children
-                : <Redirect to="/" />
-            }
-        />
-    );
+const PrivateRoute = ({ Component }) => {
+    const auth = useAuth()
+    const { location } = useLocation() 
+    if(!auth) {
+        return <Navigate to = '/login' state={{ from: location }} />
+    }   
+    return <Component />;
 }
+
+export default PrivateRoute;
