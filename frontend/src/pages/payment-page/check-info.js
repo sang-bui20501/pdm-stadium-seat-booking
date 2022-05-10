1aba0e11aea63462b37a09988eb0880ccc9d754c
luongphucdien <work.luongphucdien@gmail.com>
2022-05-10 22:09:13 +0700

    Dien | :pray: | Change the UI of Check Info page

--- frontend/src/pages/payment-page/check-info.js
+++ frontend/src/pages/payment-page/check-info.js
@@ -5,11 +5,35 @@ import { useLocation } from "react-router-dom";
 export default function CheckInfo() {
     const location = useLocation();
 
+    const proceedPayment = () => {
+
+    }
+
     return (
-        <div className="container-fluid mt-5">
-            <p> {location.state.cardNum} </p>
-            <p> {location.state.expiry} </p>
-            <p> {location.state.cvv} </p>
+        <div className="d-flex mt-5 justify-content-center">
+            <div className="card w-50 border border-primary shadow-0">
+                <h2 className="card-header text-center">Check your Information</h2>
+                <div className="card-body">
+                    <h5 className="card-text">
+                        Name: {}
+                    </h5>
+
+                    <h5 className="card-text">
+                        Email: {}
+                    </h5>
+
+                    <h5 className="card-text">
+                        Card number: {location.state.cardNum}
+                    </h5>
+                </div>
+                <div className="card-footer">
+                    <h3 className="mb-4">Price: {}</h3>
+                    <div className="text-center">
+                        <button className="btn btn-primary shadow-0 w-100" type={'button'} onClick={proceedPayment}>Pay</button>
+                    </div>
+                </div>
+                
+            </div>
         </div>
     );
 }
