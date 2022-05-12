9626c201bf97e6791c4602023ddba9c1e991c26a
Sang Bui <sang.bui@metro.digital>
2022-05-12 22:53:02 +0700

    Sang | Sign in | Update Sign in mechanism

--- backend/src/main/java/com/pdm/pdm/booking/Security/UserService.java
+++ backend/src/main/java/com/pdm/pdm/booking/Security/UserService.java
@@ -1,11 +1,11 @@
 package com.pdm.pdm.booking.Security;
 
-import java.util.ArrayList;
-
 import com.pdm.pdm.booking.Customer.Customer;
+import com.pdm.pdm.booking.Customer.CustomerRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import java.util.Arrays;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,10 +14,21 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements UserDetailsService{
-    private Customer customer;
+
+    private CustomerRepository customerRepository;
+
+    public UserService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new User(customer.getUsername(),customer.getPassword(), null);
+        Customer customer = customerRepository.findCustomerByUsername(username);
+        if(customer == null) {
+            throw new UsernameNotFoundException("Username not found");
+        }
+        GrantedAuthority authority = new SimpleGrantedAuthority("myAuthority");
+        return new User(customer.getUsername(), customer.getPassword(), Arrays.asList(authority));
     }
     
 }
