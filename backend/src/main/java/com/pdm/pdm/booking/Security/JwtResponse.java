383826218d4b9b803330e840516f9acecc22480f
nhathuy19135 <74639606+nhathuy19135@users.noreply.github.com>
2022-05-12 21:29:50 +0700

    Huy | :dizzy_face: | Token system

---
+++ backend/src/main/java/com/pdm/pdm/booking/Security/JwtResponse.java
@@ -0,0 +1,20 @@
+package com.pdm.pdm.booking.Security;
+
+public class JwtResponse {
+
+    private String jwtToken;
+
+    public JwtResponse(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+
+    public String getJwtToken() {
+        return jwtToken;
+    }
+
+    public void setJwtToken(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+
+
+}
